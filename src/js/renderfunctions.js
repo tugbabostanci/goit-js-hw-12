dcf50631a5bed35cee118c6f635e1a7bb605aef4
tugbabostanci <ttugbabostanci@gmail.com>
2025-11-14 18:39:21 +0300

    add scroll-up btn

--- src/js/renderfunctions.js
+++ src/js/renderfunctions.js
@@ -166,3 +166,27 @@ function showToast(type, message) {
         timeout: 2000,
     });
 }
+
+
+
+document.addEventListener('DOMContentLoaded', () => {
+    const btn = document.querySelector('.button-up');
+
+
+    window.addEventListener('scroll', () => {
+        if (window.pageYOffset > 300) {
+            btn.classList.add('visible');
+        } else {
+            btn.classList.remove('visible');
+        }
+    });
+
+
+
+    btn.addEventListener('click', () => {
+        window.scrollTo({
+            top: 0,
+            behavior: 'smooth'
+        });
+    });
+});
